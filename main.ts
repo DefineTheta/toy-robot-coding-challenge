8beb9fd844058ceeac8ec0f473647530ad8f9554
Parthey Bhatt <github@thetacubed.com>
2025-04-12 10:00:38 +1000

    Reading input text file and parsing the PLACE command

--- main.ts
+++ main.ts
@@ -1,8 +1,31 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
-
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  const filePath = Deno.args[0];
+  const input = await Deno.readTextFile(filePath);
+  const lines = input.split(/\r?\n/);
+
+  let dir = "";
+  let x = 0;
+  let y = 0;
+  let isRobotPlaced = false;
+
+  for (let i = 0; i < lines.length; i++) {
+    const commands = lines[i].split(" ");
+
+    if (commands.length === 0) continue;
+
+    switch (commands[0]) {
+      case "PLACE": {
+        const args = commands[1].split(",");
+
+        x = Number(args[0]);
+        y = Number(args[1]);
+        dir = args[3];
+        break;
+      }
+      default: {
+        continue;
+      }
+    }
+  }
 }
